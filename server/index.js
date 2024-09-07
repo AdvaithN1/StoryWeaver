5a492f1f671ee643e1e27fa7926e8272e5c84dba
Advaith <advaith.narayanan20@gmail.com>
2024-09-07 14:23:05 -0700

    Something

--- server/index.js
+++ server/index.js
@@ -69,60 +69,19 @@ app.get('/ping', nocache, ping)
 //   }
 // });
 
-app.post('/generate-text', async (req, res) => {
-  console.log("generate text");
-  try {
-    const { prompt } = req.body;
-    const response = await runChat();
-    console.log(response);
-    res.json(response);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Errror generating text' });
-  }
-});
+// app.post('/generate-text', async (req, res) => {
+//   console.log("generate text");
+//   try {
+//     const { prompt } = req.body;
+//     const response = await runChat();
+//     console.log(response);
+//     res.json(response);
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).json({ error: 'Errror generating text' });
+//   }
+// });
 
-async function runChat() {
-  const genAI = new GoogleGenerativeAI(API_KEY);
-  const model = genAI.getGenerativeModel({ model: MODEL_NAME });
-
-  const generationConfig = {
-    temperature: 0.9,
-    topK: 1,
-    topP: 1,
-    maxOutputTokens: 2048,
-  };
-
-  const safetySettings = [
-    {
-      category: HarmCategory.HARM_CATEGORY_HARASSMENT,
-      threshold: HarmBlockThreshold.BLOCK_NONE,
-    },
-    {
-      category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
-      threshold: HarmBlockThreshold.BLOCK_NONE,
-    },
-    {
-      category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
-      threshold: HarmBlockThreshold.BLOCK_NONE,
-    },
-    {
-      category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
-      threshold: HarmBlockThreshold.BLOCK_NONE,
-    },
-  ];
-
-  const chat = model.startChat({
-    generationConfig,
-    safetySettings,
-    history: [
-    ],
-  });
-
-  const result = await chat.sendMessage("give me a fantasy picture story on a boy and a dragon");
-  const response = result.response;
-  console.log(response.text());
-}
 
 
 
